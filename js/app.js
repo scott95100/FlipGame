713cfc3d43ae8ff7cde5797d70e097e7d7624b46
scott95100 <scott95100@gmail.com>
2021-01-26 17:04:06 -0700

    added planning for the JS logic in psedo code and declared all the functions currently identified as needed

--- js/app.js
+++ js/app.js
@@ -29,8 +29,41 @@ const generatePlayerLives = document.querySelector("button").addEventListener("c
 })
 
 
+//function that identifies when pairs are matched/identified or not
 
-//function that subtracts a life from the player board when 2 cards do not match
+const pairCompair = () => {
+    //will need to grabe both cards that are clicked
+    //change the background color of the cards clicked
+    //compair if the inner text is equal to eachother
+    //if equal
+        //triggers the generatePairWindow
+    //otherwise
+        //trigger the miss function 
+}
 
-//function that identifies when pairs are either matched or identifying a miss
+//function that generates pop-out window with the syntax information triggered when a pair is made
+const generatePairWindow = () => {
+    //create a window element with innerHTML language describing the syntax of the pair made
+    //change the background color of both cards that were paired-- with a button to close the window
+} 
+
+//function that triggers on miss and subtracts a life from the player board when 2 cards do not match and returns the original background color
+const missPair = () => {
+    //subtract 1 from the lives in the player window
+    //return the background color of both cards to original 
+}
+
+
+//function that generates the "winner" window when all pairs are made on the board
+const generateWinner = () => {
+    //create a window with innerHTML declairing game won
+    //replace all cards backgrounds to original color
+}
+
+//function that generates the game over window when lives = 0;
+const gameOver = () => {
+    //if lives === 0 
+    //generate window declaring game over 
+    //reset all backgrounds to original 
+}
 
