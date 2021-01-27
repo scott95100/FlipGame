27bfdfb7d919b26d529f487598af2b870a911e82
scott95100 <scott95100@gmail.com>
2021-01-27 15:28:28 -0700

    finished the function that selects cards. started working the comparison function

--- js/app.js
+++ js/app.js
@@ -2,7 +2,31 @@
 window.addEventListener("DOMContentLoaded", () => {
 })
 
-//global variables 
+const htmlIds = ["#DOM", "#DOMmatch", "#boolean", "#booleanMatch", "#Object", "#ObjectMatch", "#HTML", "#HTMLmatch"]
+
+const cards = document.querySelectorAll(".card")
+////test area///
+console.log(cards)
+
+const pairCompair = () => {
+    for(let j = 0; j < cards.length; j++) {
+        
+    }
+}
+
+pairCompair()
+
+
+
+
+///end test area
+
+
+
+
+
+
+
 
 
 
@@ -18,8 +42,69 @@ const clicked = document.querySelector('button').addEventListener('click', (e)=>
 ///functions
 
 
-//function to creats the inital player window information
+//function to makes the playing cards flip when clicked
 
+
+
+
+const flipCard =()=>{
+    for(let i = 0; i < htmlIds.length; i++){
+        
+        if(htmlIds[i] === "#DOM"){
+            cards[0].addEventListener('click', (e)=> {
+                document.querySelector('#DOM').style.background = 'black'
+                document.querySelector('#DOM').style.color = "white" 
+            })
+        }
+        if(htmlIds[i] === "#HTML"){
+            cards[3].addEventListener('click', (e)=> {
+                document.querySelector('#HTML').style.background = 'black'
+                document.querySelector('#HTML').style.color = "white"
+            })
+        }
+        if(htmlIds[i] === "#boolean"){
+            cards[1].addEventListener('click', (e)=> {
+                document.querySelector('#boolean').style.background = 'black'
+                document.querySelector('#boolean').style.color = "white"
+            })
+        }
+        if(htmlIds[i] === "#Object"){
+            cards[2].addEventListener('click', (e)=> {
+                document.querySelector('#Object').style.background = 'black'
+                document.querySelector('#Object').style.color = "white"
+            })
+        }
+        if(htmlIds[i] === "#booleanMatch"){
+            cards[4].addEventListener('click', (e)=> {
+                document.querySelector('#booleanMatch').style.background = 'black'
+                document.querySelector('#booleanMatch').style.color = "white"
+            })
+        }
+        if(htmlIds[i] === "#ObjectMatch"){
+            cards[5].addEventListener('click', (e)=> {
+                document.querySelector('#ObjectMatch').style.background = 'black'
+                document.querySelector('#ObjectMatch').style.color = "white"
+            })
+        }
+        if(htmlIds[i] === "#DOMmatch"){
+            cards[6].addEventListener('click', (e)=> {
+                document.querySelector('#DOMmatch').style.background = 'black'
+                document.querySelector('#DOMmatch').style.color = "white"
+            })
+        }
+        if(htmlIds[i] === "#HTMLmatch"){
+            cards[7].addEventListener('click', (e)=> {
+                document.querySelector('#HTMLmatch').style.background = 'black'
+                document.querySelector('#HTMLmatch').style.color = "white"
+            })
+        }
+        
+    }
+}
+flipCard()
+
+
+//generates the user information screen
 const generatePlayerName = 
     document.querySelector("button").addEventListener('click', (e)=> {
         document.querySelectorAll("li")[0].innerHTML = "awesome programmer"
@@ -31,15 +116,9 @@ const generatePlayerLives = document.querySelector("button").addEventListener("c
 
 //function that identifies when pairs are matched/identified or not
 
-const pairCompair = () => {
-    //will need to grabe both cards that are clicked
-    //change the background color of the cards clicked
-    //compair if the inner text is equal to eachother
-    //if equal
-        //triggers the generatePairWindow
-    //otherwise
-        //trigger the miss function 
-}
+// const pairCompair = () => {
+    
+// }
 
 //function that generates pop-out window with the syntax information triggered when a pair is made
 const generatePairWindow = () => {
